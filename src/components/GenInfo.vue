d54fdc4ea70aa1b3d17a97f0d7c4f3f3a9b2d4c0
Marco <sonni46marcosenpai@gmail.com>
2021-10-01 12:46:15 +0200

    update

--- src/components/GenInfo.vue
+++ src/components/GenInfo.vue
@@ -1,12 +1,34 @@
 <template>
-  <div class="bg-Ebb container-fluid">
+  <div class="bg-Ebb container-fluid pad-50">
         <div class="container">
             <div>
-                <ul class="d-flex p-y-15">
-                    <li class="p-y-15">
-                        <h2>WHO WE ARE</h2>
-                        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa sint ex dolorum autem non reprehenderit neque, harum vitae! Delectus laudantium provident illum ab animi, repudiandae odio voluptate non excepturi amet!</span>
-                        <p>LEARN MORE</p>
+                <ul class="d-flex bg-white border-t-Nymph">
+                    <li class="p-y-15 p-x-50">
+                        <h3 class="p-y-15 p-t-35">WHO WE ARE</h3>
+                        <span>Ipsa sint ex dolorum autem non reprehenderit neque, harum vitae!
+                            Ipsa sint ex dolorum autem non reprehenderit neque, harum vitae!
+                        </span>
+                        <div class=" p-y-35 p-b-50">
+                            <span class="button">LEARN MORE</span>
+                        </div>
+                    </li>
+                    <li class="p-y-15 p-x-50">
+                        <h3 class="p-y-15 p-t-35">WHAT WE DO </h3>
+                        <span>Ipsa sint ex dolorum autem non reprehenderit neque, harum vitae!
+                            Ipsa sint ex dolorum autem non reprehenderit neque, harum vitae!
+                        </span>
+                        <div class=" p-y-35 p-b-50">
+                            <span class="button">LEARN MORE</span>
+                        </div>
+                    </li>
+                    <li class="p-y-15 p-x-50">
+                        <h3 class="p-y-15 p-t-35">WHERE WE WORK</h3>
+                        <span>Ipsa sint ex dolorum autem non reprehenderit neque, harum vitae!
+                            Ipsa sint ex dolorum autem non reprehenderit neque, harum vitae!
+                        </span>
+                        <div class=" p-y-35 p-b-50">
+                            <span class="button">LEARN MORE</span>
+                        </div>
                     </li>
                 </ul>
             </div>
@@ -28,6 +50,20 @@ export default {
 ul {
     list-style: none;
     text-align: center;
+    border-left:1px solid $color-French-Gray;
+    border-bottom:1px solid $color-French-Gray;
+    border-right:1px solid $color-French-Gray;
+    .center {
+        border-left:1px solid $color-French-Gray;
+        border-right:1px solid $color-French-Gray;
+    }
+}
+
+.button {
+    cursor: pointer;
+    padding:10px 20px;
+    background-color:$color-Shark ;
+    color:$color-White;
 }
 
 
